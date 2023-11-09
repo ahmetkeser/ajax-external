02737d50379980ccdd477439c471f9156369f417
ahmet keser <ahmetkeser006@gmail.com>
2023-11-10 02:16:35 +0300

    bnb get request

--- app.js
+++ app.js
@@ -2,5 +2,16 @@ document.getElementById("change").addEventListener("click",change)
 
 function change(){
     const xhr = new XMLHttpRequest() // objemizi oluşturuyoruz
-    xhr.open("GET", "https://apigw.vakifbank.com.tr:8443",true) // bağlantıyı açıyoruz
-}
+    xhr.open("GET", "https://api.binance.com/api/v3/avgPrice?symbol=BNBUSDT") // bağlantıyı açıyoruz
+    xhr.onload = function(){
+        if(this.status){
+            const response = JSON.parse(this.responseText)
+            const price =response.price
+            const emount = Number(document.getElementById("amount").value)
+            document.getElementById("tl").value=emount*price
+            
+
+        }
+    }
+    xhr.send()
+} 
